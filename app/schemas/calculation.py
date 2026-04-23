800a85c1ea289d4b57100ad885c82404404b3744
VladislavMayevskiy <vladislavmaevski46@gmail.com>
2026-04-23 13:43:25 +0300

    BCALC-5 Implement the second material calculation based on floor area.

--- app/schemas/calculation.py
+++ app/schemas/calculation.py
@@ -15,3 +15,4 @@ class CalculationResponse(BaseModel):
     wall_area: float
     wall_area_with_reserve: float
     paint_liters: float
+    tile_required_sqm: float
--- app/services/calculation_service.py
+++ app/services/calculation_service.py
@@ -14,18 +14,26 @@ def perimeter(length: float, width: float ) -> float:
 def walls(perimeter_value: float, height: float, windows_area: float, doors_area: float) -> float:
     return perimeter_value * height - windows_area - doors_area
 
+def tile_required_sqm(length: float, width: float) -> float:
+    floor_area = floor(width=width, length=length)
+    reserve_multiplier = 1.1
+    tile_required = round(floor_area * reserve_multiplier, 2)
+    return tile_required
+
 
 def calculate_room(data: CalculationInput) -> CalculationResponse:
     floor_area = floor(data.length, data.width)
     ceiling_area = ceiling(data.length, data.width)
     perimeter_value = perimeter(data.length, data.width)
     wall_area = walls(perimeter_value, data.height, data.windows_area, data.doors_area)
-    wall_area_with_reserve=round(wall_area * 1.1, 2)
-    paint_liters = wall_area_with_reserve / 9
+    wall_area_with_reserve = round(wall_area * 1.1, 2)
+    paint_liters = round(wall_area_with_reserve / 9, 2) 
+    tile_required = tile_required_sqm(length=data.length, width=data.width)
     return CalculationResponse( 
         floor_area=floor_area,
         ceiling_area=ceiling_area,
         wall_area=wall_area,
         wall_area_with_reserve=wall_area_with_reserve,
-        paint_liters=paint_liters
+        paint_liters=paint_liters,
+        tile_required_sqm=tile_required
 )
